559743e422315c06700c9a85a375721a523b270e
Koliorga <groceries.squatting232@passinbox.com>
2025-07-17 12:09:25 +1200

    udpate config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'BOPRC 3D Printing Documentation',
+  tagline: 'How to print on the CreatBot D600 Pro',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -28,7 +28,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'EngineBop', // Usually your GitHub org/user name.
-  projectName: 'CreatBot-D600-Pro-Info', // Usually your repo name.
+  projectName: '3Dprint', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
